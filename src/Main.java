429a0daaba4082d40f84842af10f0be4b3a21dfa
TarasRokochyi <tarrok2005@gmail.com>
2023-02-26 12:23:27 +0200

    3 tasks

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         while (true){
+            System.out.println("Task:");
             String text = scan.nextLine();
             if(text.equals("1")){
                 while (true) {
+                    System.out.println("Go or q");
                     text = scan.nextLine();
                     if (text.equals("go")) {
                         System.out.println("Math: ");
@@ -38,17 +40,20 @@ public class Main {
             }
             else if(text.equals("2")) {
                 while (true) {
+                    System.out.println("Go or q");
                     text = scan.nextLine();
                     if (text.equals("go")) {
                         float x = scan.nextFloat();
+                        scan.nextLine();
                         float y = scan.nextFloat();
+                        scan.nextLine();
                         if(x < 0 && y < 0){
                             System.out.println("x: " + Math.abs(x) + "  y: " + Math.abs(y));
                         }
-                        if(x < 0 || y < 0){
+                        else if(x < 0 || y < 0){
                             System.out.println("x: " + (x + 1) + "  y: " + (y + 1));
                         }
-                        if((x > 2 && x < 9) && (y > 2 && y < 9)){
+                        else if((x > 2 && x < 9) && (y > 2 && y < 9)){
                             System.out.println("x: " + (x / 10) + "  y: " + (y / 10));
                         }
                         else{
@@ -65,19 +70,26 @@ public class Main {
             }
             else if(text.equals("3")){
                 while (true) {
+                    System.out.println("Go or q");
                     text = scan.nextLine();
                     if (text.equals("go")) {
                         int a = Integer.parseInt(scan.nextLine());
                         int b = Integer.parseInt(scan.nextLine());
                         int c = Integer.parseInt(scan.nextLine());
-                        if ( a == b && b == c){
-
-                        }
                         if ((a + b) < c || (a + c) < b || (c + b) < a){
                             System.out.println("Такого трикутника не існує");
                         }
-                        if(a == 0){
-
+                        else if ( a == b && b == c){
+                            System.out.println("Трикутник рівносторонній");
+                        }
+                        else if (((a*a) + (b*b) == (c*c)) || ((a*a) + (c*c) == (b*b)) || ((c*c) + (b*b) == (a*a))){
+                            System.out.println("Трикутник прямокутний");
+                        }
+                        else if (((a == b) && (a != c)) || ((a == c) && (a != b)) || ((c == b) && (c != a))){
+                            System.out.println("Трикутник рівнобедрений");
+                        }
+                        else {
+                            System.out.println("Трикутник довільний");
                         }
                     } else if (text.equals("q")) {
                         break;
@@ -85,6 +97,7 @@ public class Main {
                 }
             }
             else if(text.equals("4")){
+                System.out.println("Go or q");
                 text = scan.nextLine();
                 if (text.equals("go")){
 
@@ -94,6 +107,7 @@ public class Main {
                 }
             }
             else if(text.equals("5")){
+                System.out.println("Go or q");
                 text = scan.nextLine();
                 if (text.equals("go")){
 
@@ -103,6 +117,7 @@ public class Main {
                 }
             }
             else if(text.equals("6")){
+                System.out.println("Go or q");
                 text = scan.nextLine();
                 if (text.equals("go")){
 
